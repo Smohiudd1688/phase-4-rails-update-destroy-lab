c74e4bc639d1a8c8d23cb8b48b470e8e15f42179
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-08-20 14:11:42 -0700

    commit tree 22451b12e118ecbd26784f6f30a3ddbb8f99dcdd

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_no_response
 
   # GET /plants
   def index
@@ -18,9 +19,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end 
+
+  #DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_no_response
+    render json: {error: "plant not found"}
+  end
+
+  def find_plant
+    Plant.find(params[:id])
   end
 end
